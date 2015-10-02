92a4c12a43d739749f4228ad1eb9264f6d6375d4
Cory <cjm43@njit.edu>
2015-10-02 14:51:49 -0400

    working on colliding objects

--- include/Collision.h
+++ include/Collision.h
@@ -1,5 +1,5 @@
-#ifndef __COLLISIONS_H__
-#define __COLLISIONS_H__
+#ifndef __COLLISION_H__
+#define __COLLISION_H__
 
 #include "vector.h"
 
@@ -9,8 +9,11 @@ typedef struct
     float w,h,d;
 }Cube;
 
+#define cube_set(cube,a,b,c,l,m,n) (cube.x=a,cube.y=b,cube.z=c,\
+                                    cube.w=l,cube.h=m,cube.d=n)
+
 int cube_cube_intersection(
-	Cube a,
-	Cube b);
+    Cube a,
+    Cube b);
 
 #endif
--- include/body.h
+++ include/body.h
@@ -2,14 +2,30 @@
 #define __BODY_H__
 
 #include "vector.h"
-#include "Collision.h"
+#include "collisions.h"
+#include "mgl_callback.h"
 
 typedef struct
 {
     Vec3D position;
     Vec3D velocity;
     Cube bounds;
-    void *userData; /**<pointer back to the player*/
+    MglCallback touch;
+    int  _needsBackoff;
+    int  _done;
+    Vec3D _stepOffVector;
 }Body;
+
+/**
+ * @brief resets a body pre-physics calculation
+ * @param body the body to reset
+ */
+void body_reset(Body *body);
+
+/**
+ * @brief applies any post physics processing needed for the body
+ * @param body the body to process
+ */
+void body_process(Body *body);
 
 #endif
